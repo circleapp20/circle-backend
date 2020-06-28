fc6b081a5d72d9a5bf4c3fd32a8598bf2d8dfbee
Amo Timothy Kofi <timdereaper1@gmail.com>
2020-06-28 10:07:42 +0000

    chore: added configuration for netlify

--- src/bootstrap/startServer.ts
+++ src/bootstrap/startServer.ts
@@ -1,6 +1,6 @@
 import { Constants } from 'base/config/node/constants';
 import { errorHandlerMiddleware } from 'base/errors/node/errorHandlerMiddleware';
-import { getApiRouter, getNextRequestHandler, getNextRouter } from 'base/server/utilities';
+import { getApiRouter } from 'base/server/utilities';
 import { setupCircleDatabase } from 'core/database/createCircleDatabase';
 import cors from 'cors';
 import { Express, json, Router, urlencoded } from 'express';
@@ -9,23 +9,12 @@ import { apiRoutes } from './apiRoutes';
 export const startServer = async (app: Express, callBack?: () => void) => {
 	await setupCircleDatabase();
 
-	let handle: any;
-
-	if (process.env.NODE_ENV === 'production') {
-		handle = await getNextRequestHandler();
-	}
-
 	app.use(cors());
 	app.use(json());
 	app.use(urlencoded({ extended: false }));
 
 	// add the api routes for version 1
 	app.use('/api/v1', getApiRouter(Router(), apiRoutes));
-
-	// add next app routes
-	if (handle) {
-		app.all('*', getNextRouter(handle));
-	}
 
 	// add an error middleware to handle errors thrown during request
 	// processing, which will gracefully return the proper error response
